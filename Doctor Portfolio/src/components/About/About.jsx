5b5621d2d0e4d3c30986e8b23b55cbab4492b415
Muhammad Zeeshan Akram <161469971+Zeeshansiddiqui9790@users.noreply.github.com>
2024-08-10 03:49:17 +0500

    Added about section

---
+++ Doctor Portfolio/src/components/About/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './About.module.css';
+import { getImageUrl } from '../../utils'
+
+export const About = () => {
+  return (
+    <section className={styles.container}>
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+            <img src={getImageUrl('about/aboutImage.png')} alt="Me sitting with a laptop"
+            className={styles.aboutImage}
+            />
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon"/>
+                   <div className={styles.aboutItemText}>
+                    <h3>Frontend Developer</h3>
+                    <p>I am a Frontend Developer with 25 years of experience and proficient in mern stack</p>
+                   </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/serverIcon.png")} alt="server icon"/>
+                   <div className={styles.aboutItemText}>
+                    <h3>Backend Developer</h3>
+                    <p>I am a Frontend Developer with 25 years of experience and proficient in mern stack</p>
+                   </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursorIcon.png")} alt=" UI icon"/>
+                   <div className={styles.aboutItemText}>
+                    <h3>UI designer</h3>
+                    <p>I am a Frontend Developer with 25 years of experience and proficient in mern stack</p>
+                   </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+  )
+}
