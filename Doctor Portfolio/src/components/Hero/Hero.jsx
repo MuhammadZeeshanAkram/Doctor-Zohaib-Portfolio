24de3f30cd21784782ec6139ebfd3bc0da092388
Muhammad Zeeshan Akram <161469971+Zeeshansiddiqui9790@users.noreply.github.com>
2024-08-10 02:50:44 +0500

    Added Hero component

--- Doctor Portfolio/src/components/Hero/Hero.jsx
+++ Doctor Portfolio/src/components/Hero/Hero.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import styles from './Hero.module.css';
 import { getImageUrl } from '../../utils';
+
 
 export const Hero = () => {
   return (
-    <section>
-        <div>
-        <h1>Hi, I'm Zohaib</h1>
-        <p>hey i am react js developer with 25 years of experience and proficient in mern stack reach out if wanted</p>
-        <a href='mailto:zohaibbhai@gmail.com'>Contact Me</a>
+    <section className={styles.container}>
+        <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Zohaib</h1>
+        <p className={styles.description}>hey i am react js developer with 25 years of experience and proficient in mern stack reach out if wanted</p>
+        <a href='mailto:zohaibbhai@gmail.com' className={styles.contactBtn}>Contact Me</a>
         </div>
-        <img src={getImageUrl('hero/heroImage.png')} alt="hero image of me" />
-        
+        <img src={getImageUrl('hero/heroImage.png')} alt="hero image of me"  className={styles.heroImg}/>
+        <div className={styles.topblur}/>
+        <div className={styles.bottomblur}/>
     </section>
   )
 }
