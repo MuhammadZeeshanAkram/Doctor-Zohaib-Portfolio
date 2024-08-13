7e0699638953381aa1e7a01a5e3a9059f7657e73
Muhammad Zeeshan Akram <161469971+Zeeshansiddiqui9790@users.noreply.github.com>
2024-08-13 07:05:49 +0500

    Added pictures in about

--- Doctor Portfolio/src/components/About/About.jsx
+++ Doctor Portfolio/src/components/About/About.jsx
@@ -14,7 +14,7 @@ export const About = () => {
       <h2 className={styles.title}>Biography</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl('about/aboutImage.png')}
+          src={getImageUrl('about/biography.jpg')}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
